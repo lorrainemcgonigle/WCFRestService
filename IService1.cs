1982ae01d78d22a4ec64edc1c495a94691222892
Lorraine McGonigle <mcgonigle.lorraine@gmail.com>
2018-07-06 12:36:08 +0100

    initial commit

---
+++ IService1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]//The operations available for the service to consume
+    public interface IService1
+    {
+
+        [OperationContract]//used to expose a method as a service method and also transaction flow
+        [WebInvoke(Method ="GET", ResponseFormat =WebMessageFormat.Json, BodyStyle =WebMessageBodyStyle.Bare, UriTemplate ="GetData/{value}")]
+        string GetData(string value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]//describes the data that needs to be exchanged between provider and consumer
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
